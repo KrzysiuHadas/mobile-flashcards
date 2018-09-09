420dd0536e6625e051b16dab4c1cbb99546a4a34
krzysztofHadas <hada10@gazeta.pl>
2018-09-09 14:17:01 +0200

    completing asyncStorage, app component shows list

--- App.js
+++ App.js
@@ -1,28 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
 import { PrimaryBtn, SecondaryBtn } from './buttons/BigButtons'
-import { getDeck, saveDeckTitle, addCardToDeck } from './api/storage'
+import { getDeck, saveDeckTitle, addCardToDeck, fillStorageWithData, getDecks } from './api/storage'
 
 export default class App extends React.Component {
+
+  state = {
+    isLoading: true,
+    arrayOfDecks: []
+  }
+
+  componentDidMount() {
+    getDecks()
+      .then((decks) => {
+        const arrayOfDecks = Object.keys(decks)
+        this.setState({
+          arrayOfDecks,
+          isLoading: false
+        })
+      })
+  }
   render() {
-    const card = {
-      question: 'How old is Donald Trump?',
-      answer: "Nobody knows, he's a lizard",
+
+    if (this.state.isLoading) {
+      return <View><Text>Loading...</Text></View>
     }
-
     return (
-      <View style={styles.container}>
-          <PrimaryBtn         
-            onPress={() => {
-              saveDeckTitle("React")
-          }}
-            buttonText="Primary Btn" />
 
-          <SecondaryBtn         
-            onPress={() => {
-              getDeck("React").then((deck) => console.log(JSON.parse(deck)))
-          }}
-            buttonText="Secondary Btn"/>          
+      <View style={styles.container}>
+
+        <View style={{ maxHeight: 35, flex: 1, marginTop: 50}}>
+          <Text style={styles.header}>Choose your deck</Text>
+        </View>
+
+        <View style={{ flex: 1, width: 300, marginTop: 50 }}>
+          {<FlatList
+            data={this.state.arrayOfDecks.map((deck) => { return { "key": deck } })}
+            renderItem={({ item }) => {
+              return (
+                <View>
+                  <TouchableOpacity onPress={() => { }}>
+                    <Text style={styles.listLabel}> 
+                      {item.key}
+                    </Text>
+                  </TouchableOpacity>
+                  <View style={{borderBottomColor: 'grgba(209, 209, 209, 1)ray', borderBottomWidth: 1}} />
+                </View>
+              )
+            }}
+          />}
+        </View>
       </View>
     );
   }
@@ -31,9 +58,17 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'space-around',
+    backgroundColor: 'rgba(233, 237, 239, 1)',
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    flexDirection: 'row',
+  },
+  header: {
+    fontSize: 32,
+    fontFamily: 'Helvetica',
+
+  },
+  listLabel: {
+    fontSize: 20,
+    marginBottom: 10,
   }
 });
--- api/storage.js
+++ api/storage.js
@@ -1,49 +1,83 @@
-import React from 'react'
+// import React from 'react'
 import { AsyncStorage } from 'react-native'
 
-export const getDecks = () => {
+const DECKS_STORAGE_KEY = "Mobileflashcards:decks"
 
+const initialData = {
+  React: {
+    title: 'React',
+    questions: [
+      {
+        question: 'What is React?',
+        answer: 'A library for managing user interfaces'
+      },
+      {
+        question: 'Where do you make Ajax requests in React?',
+        answer: 'The componentDidMount lifecycle event'
+      }
+    ]
+  },
+  JavaScript: {
+    title: 'JavaScript',
+    questions: [
+      {
+        question: 'What is a closure?',
+        answer: 'The combination of a function and the lexical environment within which that function was declared.'
+      }
+    ]
+  }
+}
+
+export const getDecks = async () => {
+  try {
+    let decks = await AsyncStorage.getItem(DECKS_STORAGE_KEY)
+    return JSON.parse(decks)
+  } catch (error) {
+    console.log("There was an error fetching deck: ", error)
+  }
 }
 
 export const getDeck = async (id) => {
-    try {
-        let deck = await AsyncStorage.getItem(id)
-        return deck
-    } catch (error) {
-        console.log("There was an error fetching deck: ", error)
-    }
+  try {
+    let deck = await AsyncStorage.getItem(DECKS_STORAGE_KEY)
+    return JSON.parse(deck)[id]
+  } catch (error) {
+    console.log("There was an error fetching deck: ", error)
+  }
 }
 
-export const saveDeckTitle = async (title) => {
-    const deck = {
-        title,
-        questions: [],
-    }
+export const saveDeckTitle = (title) => {
+  const deck = {
+    title,
+    questions: [],
+  }
 
-    try {
-        await AsyncStorage.setItem(title, JSON.stringify(deck))
-    } catch (error) {
-        console.log("There was an error saving data: ", error)
-    }
+  return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
+    [title]: deck
+  }))
 }
 
-export const addCardToDeck = async (title, card) => {
-    const { question, answer } = card
-    getDeck(title)
-        .then((deck) => {
-            let deckObject = JSON.parse(deck)
-            deckObject.questions.push({question, answer})
-            saveDeckAtTitle(title, deckObject)
-                .then(() => {
-                    console.log("Data successfully saved.")
-                })
-        })
+export const addCardToDeck = (title, card) => {
+  const { question, answer } = card
+  getDeck(title)
+    .then((deck) => {
+      deck.questions.push({ question, answer })
+      return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
+        [title]: deck
+      }))
+    })
+    .catch((error) => console.log("there was an error: ", error))
 }
 
 const saveDeckAtTitle = async (title, deck) => {
-    try {
-        await AsyncStorage.setItem(title, JSON.stringify(deck))
-    } catch (error) {
-        console.log("There was an error saving data: ", error)
-    }
+  try {
+    await AsyncStorage.setItem(title, JSON.stringify(deck))
+  } catch (error) {
+    console.log("There was an error saving data: ", error)
+  }
 }
+
+export const fillStorageWithData = () => {
+  return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify(initialData))
+}
+
