e82654e4ff36968661c779158d58980e11f88b4f
krzysztofHadas <hada10@gazeta.pl>
2018-09-11 14:33:34 +0200

    Quiz component done

--- Components/Quiz.js
+++ Components/Quiz.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import { View, Text, StyleSheet, Platform, ScrollView } from 'react-native'
 import FlipCard from 'react-native-flip-card'
 import { getDeck } from '../api/storage'
+import { SecondaryBtn, RedBtn, PrimaryBtn } from '../buttons/BigButtons'
 
 export default class Quiz extends Component {
   state = {
     deck: {},
     isLoading: true,
+    counter: 0,
   }
 
   getTheDeck() {
@@ -21,37 +23,78 @@ export default class Quiz extends Component {
       })
   }
 
+  resetQuiz = () => {
+    this.refs._scrollView.scrollTo({
+      x: 0,
+      y: 0,
+      animated: true
+    })
+    this.setState({counter: 0})
+  }
   componentDidMount() {
     this.getTheDeck()
   }
 
+  increaseCounter = () => {
+    this.setState((prevState) => ({counter: prevState.counter +1}))
+  }
+
   render() {
-    
-    if (this.state.isLoading) {
+    const { deck, isLoading, counter } = this.state
+    const questions = deck.questions
+    if (isLoading) {
       return <View><Text>Loading...</Text></View>
     }
 
     const { deckName } = this.props.navigation.state.params
     return (
       <View style={styles.container}>
-        <ScrollView>
+        <ScrollView ref='_scrollView'>
           <Text style={styles.header}> {deckName} </Text>
           <View style={styles.instructions}>
             <Text style={styles.instructionText}> Carefully read the question on the card.</Text>
             <Text style={styles.instructionText}> Touch the card to flip it and confirm if your answer was right.</Text>
           </View>
-          <FlipCard style={{borderWidth: 0}}>
+
+          {questions.map(({question, answer}) => {
+            return (
+              <FlipCard key={question} style={{borderWidth: 0, flex: 1}}>
             <View style={[styles.card, styles.face]}>
-              <Text>
-                Face
+              <Text style={styles.cardContentText}>
+                {question}
               </Text>
             </View>
             <View style={[styles.card, styles.back]}>
-              <Text>
-                Back
-              </Text>
+                <Text style={styles.cardContentText}>
+                  {answer}
+                </Text>
+                <View style={styles.btnContainer}>
+                  <SecondaryBtn 
+                    buttonText='Correct'
+                    onPress={this.increaseCounter}
+                    style={{width: 200}}
+                    />
+                  <View style={{marginTop: 10}} />>
+                  <RedBtn 
+                    buttonText='Incorrect'
+                    onPress={()=> {this.forceUpdate()}}
+                    />
+                </View>
+              
             </View>
           </FlipCard>
+            )
+          })}
+          <View style={[styles.btnContainer, {marginTop: 30}]}>
+            <Text style={styles.resultLabel}>
+              Your result is: {counter}
+            </Text>
+            <View style={{marginTop: 20}} />
+            <PrimaryBtn
+              buttonText='Reset'
+              onPress={this.resetQuiz}
+              />
+          </View>
         </ScrollView>
       </View>
     )
@@ -93,11 +136,25 @@ const styles = StyleSheet.create({
     marginLeft: 50,
     marginRight: 50,
     marginTop: 10,
-    
   },
   instructionText: {
     textAlign: 'center',
     color: 'rgba(97, 99, 102, 1)',
     fontSize: 11,
+  },
+  btnContainer: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginBottom: 15,
+  },
+  cardContentText: {
+    fontSize: 32,
+    textAlign: 'center',
+    marginTop: 40,
+  },
+  resultLabel: {
+    fontSize: 20,
+
   }
 })
